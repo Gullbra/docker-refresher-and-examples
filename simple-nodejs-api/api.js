8f7238270a70b87c32c5722b6cde2b71028b4d21
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-02-06 20:34:17 +0100

    simple nodejs api

---
+++ simple-nodejs-api/api.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.ENV_API_PORT || 3080
+
+app
+  .route('*')
+  .get((req, res) => {
+    res.send('Hello World!')
+  })
+
+app.listen(port, () => console.log(`📡 listening to http://localhost:${port}`))
